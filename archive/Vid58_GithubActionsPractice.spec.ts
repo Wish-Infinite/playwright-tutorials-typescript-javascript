0ddfed32ad4ba627a221eb0609ebb4bccc9a3c8b
Wish-Infinite <testcodeautomate@gmail.com>
2024-11-03 01:57:50 +0530

    Added Azure DevOps Pipeline Video Scripts and fixed await issue for Vide 58,59,60

--- archive/Vid58_GithubActionsPractice.spec.ts
+++ archive/Vid58_GithubActionsPractice.spec.ts
@@ -5,7 +5,7 @@ test("Practice Test 1", async ({ page }) => {
     console.log("Starting Practice Test 1");
     await page.goto("https://www.saucedemo.com/");
     console.log(await page.title());
-    expect(page).toHaveTitle("Swag Labs")
+    await expect(page).toHaveTitle("Swag Labs")
     console.log("Ending Practice Test 1");
 })
 
@@ -13,7 +13,7 @@ test("Practice Test 2", async ({ page }) => {
     console.log("Starting Practice Test 2");
     await page.goto("https://www.saucedemo.com/");
     console.log(await page.title());
-    expect(page).toHaveTitle("Swag Labs")
+    await expect(page).toHaveTitle("Swag Labs")
     console.log("Ending Practice Test 2");
 })
 
@@ -21,7 +21,7 @@ test("Practice Test 3", async ({ page }) => {
     console.log("Starting Practice Test 3");
     await page.goto("https://www.saucedemo.com/");
     console.log(await page.title());
-    expect(page).toHaveTitle("Swag Labs wrong")
+    await expect(page).toHaveTitle("Swag Labs wrong")
     console.log("Ending Practice Test 3");
 })
 
--- tests/temp.spec.ts
+++ tests/temp.spec.ts
@@ -3,4 +3,10 @@ import {test, expect} from "@playwright/test"
 test("Temp",async({page})=>{
     await page.goto("https://www.google.com")
     console.log(await page.title())
+
+    await page.goto('https://artoftesting.com/samplesiteforselenium');
+    await page.locator('#testingDropdown').selectOption('Manual');
+    await expect(page.locator('#testingDropdown')).toHaveValue('Manual');
+    const val = await page.locator('#testingDropdown').inputValue()
+    console.log(val)
 })
