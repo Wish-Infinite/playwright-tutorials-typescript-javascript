f2f68593ccc5c27215fc3e90e612fb6e35aab3d6
Wish-Infinite <testcodeautomate@gmail.com>
2025-12-07 05:07:03 +0530

    Updated code and included archive folder as test directry

--- archive/Vid63_PlaywrightCLIPractice2.spec.ts
+++ archive/Vid63_PlaywrightCLIPractice2.spec.ts
@@ -7,7 +7,7 @@ test("Playwright CLI Practice Test 4",async({page})=>{
     console.log("Ending Practice Test 4");
 })
 
-test("Playwright CLI Practice Test 5 @UAT",async({page})=>{
+test("Playwright Practice CLI ,Test 5 @UAT",async({page})=>{
     console.log("Starting Practice Test 5");
     await page.goto("https://www.google.com");
     await expect(page).toHaveTitle("Google");
--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ import path from 'path';
 //   path: process.env.TEST_ENV ? `./env-files/.env.${process.env.TEST_ENV}` : `./env-files/.env.dev`
 // })
 dotenv.config({
-  path: path.resolve('testdata','.env')//"./testdata/.env"
+  path: path.resolve('testdata', '.env')//"./testdata/.env"
 })
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -22,7 +22,7 @@ export default defineConfig({
   //grep:/UI/,
   //grepInvert:/sanity/,
   //globalSetup: "./global-setup.ts",
-  testDir: './tests',
+  testDir: './',
   // expect:{
   //   toHaveScreenshot:{
   //     maxDiffPixels:20,
@@ -34,7 +34,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 1 : 0,   
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -48,15 +48,15 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-    baseURL:"https://restful-booker.herokuapp.com",
-    extraHTTPHeaders:{
-      Accept:"application/json",
-      "Content-Type":"application/json",
+    baseURL: "https://restful-booker.herokuapp.com",
+    extraHTTPHeaders: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
       // Authorization: "Basic YWRtaW46cGFzc3dvcmQxMjM="
     },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    screenshot:'only-on-failure',
-    video:'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     // trace:'on',
     //storageState:"./playwright/.auth/auth.json",
   },
